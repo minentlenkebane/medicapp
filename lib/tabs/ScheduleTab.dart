4bb10364638f374615bf5972126243662581ad3c
minentlenkebane <minentlenkebane@gmail.com>
2022-11-14 04:57:00 -0800

    new version

---
+++ lib/tabs/ScheduleTab.dart
@@ -0,0 +1,307 @@
+import 'package:flutter/material.dart';
+import 'package:medicare/styles/colors.dart';
+import 'package:medicare/styles/styles.dart';
+
+class ScheduleTab extends StatefulWidget {
+  const ScheduleTab({Key? key}) : super(key: key);
+
+  @override
+  State<ScheduleTab> createState() => _ScheduleTabState();
+}
+
+enum FilterStatus { Upcoming, Complete, Cancel }
+
+List<Map> schedules = [
+  {
+    'img': 'assets/doctor06.jpeg',
+    'doctorName': 'Dr. Vatiswa Mqikela',
+    'doctorTitle': 'Dental Specialist',
+    'reservedDate': 'Wednesday, Nov 29',
+    'reservedTime': '11:00 - 12:00',
+    'status': FilterStatus.Upcoming
+  },
+  {
+    'img': 'assets/doctor07.jpg',
+    'doctorName': 'Dr. Khanyile',
+    'doctorTitle': 'Skin Specialist',
+    'reservedDate': 'Monday, Nov 14',
+    'reservedTime': '11:00 - 12:00',
+    'status': FilterStatus.Upcoming
+  },
+  {
+    'img': 'assets/doctor08.jpg',
+    'doctorName': 'Dr. Nzoyiya',
+    'doctorTitle': 'General Specialist',
+    'reservedDate': 'Thursday, Dec 29',
+    'reservedTime': '11:00 - 12:00',
+    'status': FilterStatus.Upcoming
+  },
+  {
+    'img': 'assets/doctor04.jpeg',
+    'doctorName': 'Dr. John Doe',
+    'doctorTitle': 'Something Specialist',
+    'reservedDate': 'Monday, Dec 29',
+    'reservedTime': '11:00 - 12:00',
+    'status': FilterStatus.Complete
+  },
+  {
+    'img': 'assets/doctor05.jpeg',
+    'doctorName': 'Dr. Sam Smithh',
+    'doctorTitle': 'Other Specialist',
+    'reservedDate': 'Tuesday, Nov 15',
+    'reservedTime': '11:00 - 12:00',
+    'status': FilterStatus.Cancel
+  },
+  {
+    'img': 'assets/doctor05.jpeg',
+    'doctorName': 'Dr. Sam Smithh',
+    'doctorTitle': 'Other Specialist',
+    'reservedDate': 'Monday, Jan 29',
+    'reservedTime': '11:00 - 12:00',
+    'status': FilterStatus.Cancel
+  },
+];
+
+class _ScheduleTabState extends State<ScheduleTab> {
+  FilterStatus status = FilterStatus.Upcoming;
+  Alignment _alignment = Alignment.centerLeft;
+
+  @override
+  Widget build(BuildContext context) {
+    List<Map> filteredSchedules = schedules.where((var schedule) {
+      return schedule['status'] == status;
+    }).toList();
+
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.only(left: 30, top: 30, right: 30),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              'Schedule',
+              textAlign: TextAlign.center,
+              style: kTitleStyle,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Stack(
+              children: [
+                Container(
+                  width: double.infinity,
+                  height: 40,
+                  decoration: BoxDecoration(
+                    color: Color(MyColors.bg),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      for (FilterStatus filterStatus in FilterStatus.values)
+                        Expanded(
+                          child: GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                if (filterStatus == FilterStatus.Upcoming) {
+                                  status = FilterStatus.Upcoming;
+                                  _alignment = Alignment.centerLeft;
+                                } else if (filterStatus ==
+                                    FilterStatus.Complete) {
+                                  status = FilterStatus.Complete;
+                                  _alignment = Alignment.center;
+                                } else if (filterStatus ==
+                                    FilterStatus.Cancel) {
+                                  status = FilterStatus.Cancel;
+                                  _alignment = Alignment.centerRight;
+                                }
+                              });
+                            },
+                            child: Center(
+                              child: Text(
+                                filterStatus.name,
+                                style: kFilterStyle,
+                              ),
+                            ),
+                          ),
+                        ),
+                    ],
+                  ),
+                ),
+                AnimatedAlign(
+                  duration: Duration(milliseconds: 200),
+                  alignment: _alignment,
+                  child: Container(
+                    width: 100,
+                    height: 40,
+                    decoration: BoxDecoration(
+                      color: Color(MyColors.primary),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Center(
+                      child: Text(
+                        status.name,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: filteredSchedules.length,
+                itemBuilder: (context, index) {
+                  var _schedule = filteredSchedules[index];
+                  bool isLastElement = filteredSchedules.length + 1 == index;
+                  return Card(
+                    margin: !isLastElement
+                        ? EdgeInsets.only(bottom: 20)
+                        : EdgeInsets.zero,
+                    child: Padding(
+                      padding: EdgeInsets.all(15),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          Row(
+                            children: [
+                              CircleAvatar(
+                                backgroundImage: AssetImage(_schedule['img']),
+                              ),
+                              SizedBox(
+                                width: 10,
+                              ),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    _schedule['doctorName'],
+                                    style: TextStyle(
+                                      color: Color(MyColors.header01),
+                                      fontWeight: FontWeight.w700,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 5,
+                                  ),
+                                  Text(
+                                    _schedule['doctorTitle'],
+                                    style: TextStyle(
+                                      color: Color(MyColors.grey02),
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            height: 15,
+                          ),
+                          DateTimeCard(),
+                          SizedBox(
+                            height: 15,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Expanded(
+                                child: OutlinedButton(
+                                  child: Text('Cancel'),
+                                  onPressed: () {},
+                                ),
+                              ),
+                              SizedBox(
+                                width: 20,
+                              ),
+                              Expanded(
+                                child: ElevatedButton(
+                                  child: Text('Reschedule'),
+                                  onPressed: () => {},
+                                ),
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class DateTimeCard extends StatelessWidget {
+  const DateTimeCard({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Color(MyColors.bg03),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      width: double.infinity,
+      padding: EdgeInsets.all(20),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              Icon(
+                Icons.calendar_today,
+                color: Color(MyColors.primary),
+                size: 15,
+              ),
+              SizedBox(
+                width: 5,
+              ),
+              Text(
+                'Mon, July 29',
+                style: TextStyle(
+                  fontSize: 12,
+                  color: Color(MyColors.primary),
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Icon(
+                Icons.access_alarm,
+                color: Color(MyColors.primary),
+                size: 17,
+              ),
+              SizedBox(
+                width: 5,
+              ),
+              Text(
+                '11:00 ~ 12:10',
+                style: TextStyle(
+                  color: Color(MyColors.primary),
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
